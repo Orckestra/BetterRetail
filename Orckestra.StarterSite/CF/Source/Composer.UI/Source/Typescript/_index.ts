1d30aa3f38e6c1162c193471215b9e1cd7d4cbdd
pguillaume <pguillaume@gmail.com>
2019-02-15 08:34:20 -0500

    Fix for TypeScript update!
    The plugins were not referenced anywhere in the code... so the new way to build the JavaScript just skipped them.

--- Orckestra.StarterSite/CF/Source/Composer.UI/Source/Typescript/_index.ts
+++ Orckestra.StarterSite/CF/Source/Composer.UI/Source/Typescript/_index.ts
@@ -1,3 +1,6 @@
+/**
+ * Reference Handlebars helpers and plugins (they were not referenced anywhere)
+ */
 ///<reference path='./Handlebars/Helpers/EscapeHelper.ts' />
 ///<reference path='./Handlebars/Helpers/IfEqualsHelper.ts' />
 ///<reference path='./Handlebars/Helpers/IfExistsHelper.ts' />
@@ -10,3 +13,12 @@
 ///<reference path='./Handlebars/Helpers/IHandlebarsLocalization.ts' />
 ///<reference path='./Handlebars/Helpers/LocalizeFormatHelper.ts' />
 ///<reference path='./Handlebars/Helpers/LocalizeHelper.ts' />
+
+///<reference path='./Plugins/AccessibleMegaMenuPlugin.ts' />
+///<reference path='./Plugins/AntiIFrameClickJackingPlugin.ts' />
+///<reference path='./Plugins/ComposerValidationPlugin.ts' />
+///<reference path='./Plugins/FocusElementPlugin.ts' />
+///<reference path='./Plugins/HelpBubblesPlugin.ts' />
+///<reference path='./Plugins/ParsleyAccessibilityPlugin.ts' />
+///<reference path='./Plugins/SlickCarouselPlugin.ts' />
+///<reference path='./Plugins/StickyAffixPlugin.ts' />
