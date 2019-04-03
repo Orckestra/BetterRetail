c9fb08a66f6f3bb4a80b8c2f45978efa4342a60b
David Abitbol <dabitbol@gmail.com>
2019-04-03 15:47:18 -0400

    Update BuildAndPackageCF-vsts.ps1

--- BuildAndPackageCF-vsts.ps1
+++ BuildAndPackageCF-vsts.ps1
@@ -130,7 +130,7 @@ function MSDeploy-ContentToPackage {
 	"package: $PackagedWebSite"
 	
 	$msDeployExePath = Get-MsDeployLocation -regKeyPath $MsDeployInstallPathRegKey
-	$tmp = " -verb:sync -source:contentpath=`"{0}`" -dest:package=`"{1}`"" -f $FileSystemPublishedWebSite, $PackagedWebSite
+	$tmp = "-verb:sync -source:contentpath=`"{0}`" -dest:package=`"{1}`"" -f $FileSystemPublishedWebSite, $PackagedWebSite
 	
 	& $msDeployExePath $tmp 
 
