b69d8369eec735bca8fb208074be04e1ed098241
Billy Lao <billy.lao@orckestra.com>
2019-04-26 10:46:39 -0400

    basic endpoint to have all stores for pickUpLocalation

---
+++ Orckestra.StarterSite/CF/Source/Composer.Store/Parameters/GetStoresForInStorePickupViewModelParam.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Orckestra.Composer.Store.Parameters
+{
+    public class GetStoresForInStorePickupViewModelParam
+    {
+        public string Scope { get; set; }
+        public CultureInfo CultureInfo { get; set; }
+        public string BaseUrl { get; set; }
+    }
+}
