a33d169639c00e9bf7c0d3cae74f6f2ea3a0649f
Inna Boitsun <inna.boitsun@orckestra.com>
2019-05-02 11:48:42 +0300

    Add missing file

---
+++ Orckestra.StarterSite/CF/Source/Composer/IPerformanceDataCollector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orckestra.Composer
+{
+    /// <summary>
+    /// Enables collecting performance data such as response time and memory usage.
+    /// </summary>
+    public interface IPerformanceDataCollector
+    {
+        IDisposable Measure(string routineDescription);
+    }
+}
