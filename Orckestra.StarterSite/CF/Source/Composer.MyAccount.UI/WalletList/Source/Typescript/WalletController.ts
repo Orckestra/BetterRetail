f32da526d42735262306e0c9badac024448a8692
Carl Ratelle <carl.ratelle@orckestra.com>
2019-05-30 11:34:28 -0400

    delete credit card

--- Orckestra.StarterSite/CF/Source/Composer.MyAccount.UI/WalletList/Source/Typescript/WalletController.ts
+++ Orckestra.StarterSite/CF/Source/Composer.MyAccount.UI/WalletList/Source/Typescript/WalletController.ts
@@ -2,7 +2,7 @@
 ///<reference path='../../../../Composer.UI/Source/TypeScript/Mvc/Controller.ts' />
 ///<reference path='../../../../Composer.UI/Source/TypeScript/Mvc/IControllerActionContext.ts' />
 ///<reference path='../../../MyAccount/Source/Typescript/MyAccountController.ts' />
-///<reference path='../../../../Composer.Cart.UI/MonerisPaymentProvider/Source/TypeScript/MonerisCanadaPaymentProvider.ts' />
+///<reference path='../../../../Composer.Cart.UI/MonerisPaymentProvider/Source/TypeScript/MonerisPaymentService.ts' />
 
 module Orckestra.Composer {
     export class WalletController extends Orckestra.Composer.MyAccountController {
@@ -10,8 +10,8 @@ module Orckestra.Composer {
         protected monerisPaymentService: MonerisPaymentService = new MonerisPaymentService();
 
         /**
-            * Requires the element in action context to have a data-creditcard-id.
-            */
+        * Requires the element in action context to have a data-creditcard-id and data-paymentmethod-provider.
+        */
         public setDefaultCreditCard(actionContext: IControllerActionContext): void {
 
             var $paymentMethodId: JQuery = $(actionContext.elementContext).closest('[data-paymentmethod-id]');
@@ -20,15 +20,27 @@ module Orckestra.Composer {
             var $paymentMethodProviderName: JQuery = $(actionContext.elementContext).closest('[data-paymentmethod-provider]');
             var paymentMethodProviderName = $paymentMethodProviderName.data('paymentmethod-provider');
 
-            var busy = this.asyncBusy({ elementContext: actionContext.elementContext, containerContext: $paymentMethodId });
-
             this.monerisPaymentService.setDefaultCustomerPaymentMethod({
                 PaymentProviderName: paymentMethodProviderName,
                 PaymentMethodId: paymentMethodId
             })
-            .then(result => location.reload(), reason => console.error(reason))
-            .fin(() => busy.done())
-            .done();
+                .then(result => location.reload(), reason => console.error(reason))
+                .done();
+        }
+
+        /**
+        * Requires the element in action context to have a data-creditcard-id and data-paymentmethod-provider.
+        */
+        public removeCreditCard(actionContext: IControllerActionContext): void {
+            var $paymentMethodId: JQuery = $(actionContext.elementContext).closest('[data-paymentmethod-id]');
+            var paymentMethodId = $paymentMethodId.data('paymentmethod-id');
+
+            var $paymentMethodProviderName: JQuery = $(actionContext.elementContext).closest('[data-paymentmethod-provider]');
+            var paymentMethodProviderName = $paymentMethodProviderName.data('paymentmethod-provider');
+
+            this.monerisPaymentService.removePaymentMethod(paymentMethodId, paymentMethodProviderName)
+                .then(result => location.reload(), reason => console.error(reason))
+                .done();
         }
     }
 }
