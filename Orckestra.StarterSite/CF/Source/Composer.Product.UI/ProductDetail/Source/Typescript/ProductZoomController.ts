38f9d95bf90dc4bf66f34447b9ef97760c12bff0
Billy Lao <billy.lao@orckestra.com>
2019-11-18 17:26:06 -0500

    Same Zoom XL image is displayed for all variants of a product AB#27279

--- Orckestra.StarterSite/CF/Source/Composer.Product.UI/ProductDetail/Source/Typescript/ProductZoomController.ts
+++ Orckestra.StarterSite/CF/Source/Composer.Product.UI/ProductDetail/Source/Typescript/ProductZoomController.ts
@@ -3,10 +3,18 @@
 module Orckestra.Composer {
     export class ProductZoomController extends Orckestra.Composer.ProductController {
 
+        private allImages = {};
+
         public initialize() {
 
             super.initialize();
             this.initZoom();
+
+            this.eventHub.subscribe('productDetailSelectedVariantIdChanged', (e) => this.updateModalImages(e));
+            let vm = this.context.viewModel;
+            for (let variant of vm.allVariants) {
+                this.allImages[variant.Id] = variant.Images;
+            }
         }
 
         protected openZoom(event: JQueryEventObject) {
@@ -48,6 +56,25 @@ module Orckestra.Composer {
 
             // Select first thumbnail
             $('.js-zoom-thumbnail').eq(0).click();
+        }
+
+        protected updateModalImages(e) {
+            if (e.data.initialSelectedVariantId !== e.data.selectedVariantId) {
+                let modalThumbs = $('.js-zoom-thumbnail');
+                let variantImages = this.allImages[e.data.selectedVariantId];
+                if (!_.isNull(variantImages) && !_.isEmpty(variantImages)) {
+                    // by default the platform sets 4 pictures for each product
+                    modalThumbs.each(function (i) {
+                        let link = $(this);
+                        let image = $(this).find('img');
+                        let idx = link.attr('data-index');
+                        link.css({ 'display': 'none' });
+                        // images are already ordered by sequence number
+                        link.attr('data-image', variantImages[idx].ProductZoomImageUrl);
+                        image.attr('src', variantImages[idx].ThumbnailUrl);
+                    });
+                }
+            }
         }
     }
 }
