e7209273ae3699ec8b409c0a6346f1522aaf9bcf
Inna Boitsun <inna.boitsun@orckestra.com>
2020-02-18 13:45:53 +0200

    Extend CustomerSummary with all possible fields

--- src/Orckestra.Composer.Cart/ViewModels/CustomerSummaryViewModel.cs
+++ src/Orckestra.Composer.Cart/ViewModels/CustomerSummaryViewModel.cs
@@ -10,5 +10,7 @@ namespace Orckestra.Composer.Cart.ViewModels
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phone { get; set; }
     }
 }
