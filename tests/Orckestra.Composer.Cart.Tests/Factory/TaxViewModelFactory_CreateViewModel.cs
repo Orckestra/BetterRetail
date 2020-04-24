894e0b0280b91204d504ac1cbd6e0dc910dbfa6d
vadym-hyryn <57723696+vadym-hyryn@users.noreply.github.com>
2020-04-24 12:12:19 +0300

    Bugs/unit tests fixes (#243)
    * Update CartService_AddLineItemAsync.cs
    
    Quantity cannot be zero, with GetRandom.PositiveInt() possible zero values
    
    * Update BaseTestForAutocreatedSutOfType.cs
    
    Try-catch seems for me reduntant
    
    * Deleted project Orckestra.Composer.LoadTest and *.Tests from solution
    
    Initially, these projects where deleted from the solution. I included them back for investigation, but after investigation is still unclear their purpose. Therefore excluded them again not to waste processor time on rebuild. Keeping source code just for case.
    
    * Bug fixing, extending unit tests
    
    1. Added additional unit tests to cover exception and data changing cases
    2. Bug fixing for a case if total tax value is zero
    3. Unit tests were divided to cover separate situations as Microsoft recomends
    4. Dynamic data excluded as Microsoft recomends

--- tests/Orckestra.Composer.Cart.Tests/Factory/TaxViewModelFactory_CreateViewModel.cs
+++ tests/Orckestra.Composer.Cart.Tests/Factory/TaxViewModelFactory_CreateViewModel.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FizzWare.NBuilder.Generators;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Moq.AutoMock;
 using NUnit.Framework;
 using Orckestra.Composer.Cart.Factory;
-using Orckestra.Composer.Cart.Parameters;
 using Orckestra.Composer.Cart.Tests.Mock;
-using Orckestra.Composer.Providers.Dam;
 using Orckestra.Composer.ViewModels;
 using Orckestra.Overture.ServiceModel.Orders;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace Orckestra.Composer.Cart.Tests.Factory
 {
+    [TestFixture]
+    [TestOf(nameof(TaxViewModelFactory.CreateTaxViewModels))]
     public class TaxViewModelFactory_CreateViewModel
     {
         private readonly IViewModelMapper _mapper;
-        private List<Tax> _taxes;
+        private readonly CultureInfo _invariantCulture = CultureInfo.InvariantCulture;
 
         public AutoMocker Container { get; set; }
 
@@ -35,40 +32,105 @@ namespace Orckestra.Composer.Cart.Tests.Factory
             Container = new AutoMocker();
             Container.Use(_mapper);
             Container.Use(LocalizationProviderFactory.Create());
-
-            _taxes = new List<Tax>();
         }
 
-        [Test]
-        public void WHEN_cart_has_taxes_SHOULD_map_first_shipment_taxes()
+        [Test, Description("In case to pass null to list of taxes, method should not throw")]
+        public void WHEN_null_taxes_SHOULD_not_throw()
         {
-            //Arrange
-            var culture = CultureInfo.InvariantCulture;
+            var instance = Container.CreateInstance<TaxViewModelFactory>();
+            Action act = () => instance.CreateTaxViewModels(null, _invariantCulture).ToList();
+            act.ShouldNotThrow();
+        }
 
+        [Test, Description("In case to pass null to culture info, method should throw NullReferenceException")]
+        public void WHEN_null_culture_info_SHOULD_throw_null_reference_exception()
+        {
             var taxes = new List<Tax>()
             {
-                FakeCartFactory.CreateTax(_taxes, culture),
-                FakeCartFactory.CreateTax(_taxes, culture),
-                FakeCartFactory.CreateTax(_taxes, culture)
+                FakeCartFactory.CreateTax(_invariantCulture, 1, "CODE01", "DISP01")
+            };
+            var instance = Container.CreateInstance<TaxViewModelFactory>();
+            Action act = () => instance.CreateTaxViewModels(taxes, null).ToList();
+            act.ShouldThrow<NullReferenceException>();
+        }
+
+        [Test, Description("In case to pass a tax with positive total, a model should be created for it")]
+        public void WHEN_positive_tax_total_SHOULD_return_model()
+        {
+            var taxes = new List<Tax>()
+            {
+                FakeCartFactory.CreateTax(_invariantCulture, 1, "CODE01", "DISP01")
+            };
+            var instance = Container.CreateInstance<TaxViewModelFactory>();
+            var models = instance.CreateTaxViewModels(taxes, _invariantCulture).ToList();
+            models.Should().NotBeNull();
+            models.FirstOrDefault().Should().NotBeNull();
+        }
+
+
+        [Test, Description("Total tax amount and percentage should not be modified in returned model")]
+        [TestCase(1, 10)]
+        [TestCase(0.01, 1)]
+        [TestCase(10, 0.01)]
+        [TestCase(int.MaxValue, 100)]
+        public void WHEN_positive_tax_total_SHOULD_not_modify_total_amount_and_percentage(double? amount, double percentage)
+        {
+            var baseTax = FakeCartFactory.CreateTax(_invariantCulture, amount, "CODE01", "DISP01");
+            baseTax.Percentage = percentage;
+            List<Tax> taxes = new List<Tax>()
+            {
+                baseTax
             };
 
-            var sut = Container.CreateInstance<TaxViewModelFactory>();
+            var instance = Container.CreateInstance<TaxViewModelFactory>();
+            var models = instance.CreateTaxViewModels(taxes, _invariantCulture).ToList();
+            models.Count().Should().Be(1);
 
-            //Act
-            var vm = sut.CreateTaxViewModels(taxes, culture);
+            var modelTax = models.First();
+            modelTax.Percentage.Should().Be(percentage.ToString());
+            modelTax.TaxTotal.Should().Be((decimal)amount);
+        }
 
-            //Assert
-            vm.Should().NotBeNull();
-            vm.Should().HaveSameCount(_taxes, "only taxes from first shipment are considered");
 
-            for (int i = 0; i < vm.ToList().Count; i++)
+        [Test, Description("In case to pass a tax with zero, minus or null total value, do not create models for them")]
+        [TestCase(0, -1, -0.1, ExpectedResult = 0)]
+        [TestCase(null, 1, -1, ExpectedResult = 1)]
+        [TestCase(1, 0, 0.1, ExpectedResult = 2)]
+        [TestCase(1, 1, 1, ExpectedResult = 3)]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, int.MinValue, ExpectedResult = 0)]
+        public int WHEN_null_zero_negative_tax_total_SHOULD_not_return_models_for_them(double? amount1, double? amount2, double? amount3)
+        {
+            List<Tax> taxes = new List<Tax>()
             {
-                var tax = vm.ToList()[i];
-                var taxReference = _taxes[i];
+                FakeCartFactory.CreateTax(_invariantCulture, amount1, "CODE01", "DISP01"),
+                FakeCartFactory.CreateTax(_invariantCulture, amount2, "CODE02", "DISP02"),
+                FakeCartFactory.CreateTax(_invariantCulture, amount3, "CODE03", "DISP03")
+            };
+            var instance = Container.CreateInstance<TaxViewModelFactory>();
+            var models = instance.CreateTaxViewModels(taxes, _invariantCulture).ToList();
+            return models.Count();
+        }
 
-                tax.Should().NotBeNull();
-                tax.DisplayName.Should().Be(taxReference.DisplayName.GetLocalizedValue(culture.Name));
-                tax.TaxTotal.Should().HaveValue("TaxTotal was defined in tax");
+        [Test, Description("Tax display name should not be changed in models with invariant culture")]
+        [TestCase("Value added tax", "VALUE ADDED TAX", "value added tax")]
+        [TestCase("Taxe sur la valeur ajoutée", "TAXE SUR LA VALEUR AJOUTÉE", "taxe sur la valeur ajoutée")]
+        public void WHEN_invariant_culture_SHOULD_not_modify_display_name(string locValue1, string locValue2, string locValue3)
+        {
+            List<Tax> taxes = new List<Tax>()
+            {
+                FakeCartFactory.CreateTax(_invariantCulture, 1, "CODE01", locValue1),
+                FakeCartFactory.CreateTax(_invariantCulture, 1, "CODE02", locValue2),
+                FakeCartFactory.CreateTax(_invariantCulture, 1, "CODE03", locValue3)
+            };
+            var instance = Container.CreateInstance<TaxViewModelFactory>();
+            var models = instance.CreateTaxViewModels(taxes, _invariantCulture).ToList();
+
+            for (int i = 0; i < models.Count; i++)
+            {
+                var currentTax = models[i];
+                var taxReference = taxes[i];
+                currentTax.DisplayName.Should().Be(taxReference.DisplayName.GetLocalizedValue(_invariantCulture.Name));
             }
         }
     }
--- tests/Orckestra.Composer.LoadTest/readme.txt
+++
@@ -1,2 +0,0 @@
-﻿This project was excluded from the solution, but present in solution folder. Do we need it? 
-I included it back to the solution not to lost it
