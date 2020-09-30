e483e2cca5ac57e53efdee035dc28e4d1a3e6fda
Inna Boitsun <inna.boitsun@orckestra.com>
2020-09-30 12:51:58 +0300

    Update Prefered Store Request

---
+++ src/Orckestra.Composer/Parameters/UpdateUserPreferredStoreParam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Orckestra.Composer.Parameters
+{
+	/// <summary>
+	/// Parameters container to update preferred store of a customer
+	/// </summary>
+	public class UpdateUserPreferredStoreParam
+	{
+		/// <summary>
+		/// Guid of a customer
+		/// </summary>
+		public Guid CustomerId { get; set; }
+		/// <summary>
+		/// Identifier of a scope
+		/// </summary>
+		public string ScopeId { get; set; }
+		/// <summary>
+		/// Guid of a preferred store
+		/// </summary>
+		public Guid StoreId { get; set; }
+		/// <summary>
+		/// Number of a preferred store
+		/// </summary>
+		public string StoreNumber { get; set; }
+	}
+}
