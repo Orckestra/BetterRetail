d591de1cba47ad8a09eb6acea1b40a163158de7f
Taras Nakonechnyi <taras.nakonechnyi@orckestra.com>
2021-09-28 12:34:57 +0300

    Automatically build the navigation structure and category pages (#84)
    * Automatically build the navigation structure and category pages

---
+++ src/Orckestra.Composer.C1.PreConfiguration/Builders/ICategoryAndNavigationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orckestra.Composer.CompositeC1.Builders
+{
+    public interface ICategoryAndNavigationBuilder
+    {
+        void ReBuildCategoriesAndMenu(Dictionary<string, string> localizedDisplayNames);
+    }
+}
