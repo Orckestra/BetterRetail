30517567a9194f7c80c1df46227a41b6de8c34e4
oleksandr-honcharuk <55245278+oleksandr-honcharuk@users.noreply.github.com>
2021-10-08 16:23:07 +0300

    Features/44321 autocomplete vue (#96)
    * vue-autosuggest lib
    
    * highlight words
    
    * no found result
    
    * actions for all type of suggestions
    
    * remove typeahead
    
    * fix styling
    
    * fix PR
    
    * fix styling
    
    * refactor
    
    Co-authored-by: Alex Honcharuk <honcharuk.alexander@gmail.com>

---
+++ src/Orckestra.Composer.Website/UI.Package/Typings/vue-autosuggest/umd.d.ts
@@ -0,0 +1,11 @@
+import * as V from "./index";
+
+declare namespace VueAutosuggest {
+    export type VueAutosuggest = V.VueAutosuggest;
+}
+
+declare class VueAutosuggest extends V.default {}
+
+export = VueAutosuggest;
+
+export as namespace VueAutosuggest;
