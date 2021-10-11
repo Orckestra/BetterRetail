e9a31305d883b54c64dcc40b9c1c5d215f2e3125
Inna Boitsun <inna.boitsun@orckestra.com>
2021-10-11 14:24:44 +0300

    Sensitive information in GET requests on error (#97)
    * Add method=post to all forms working with password
    Fix MustBeAnonymousAttribute attribute
    AB#52172

--- src/Orckestra.Composer.Website/UI.Package/Typescript/Composer.MyAccount/ChangePassword/ChangePasswordController.ts
+++ src/Orckestra.Composer.Website/UI.Package/Typescript/Composer.MyAccount/ChangePassword/ChangePasswordController.ts
@@ -10,6 +10,7 @@ module Orckestra.Composer {
     export class ChangePasswordController extends Orckestra.Composer.MyAccountController {
 
         protected membershipService: IMembershipService = new MembershipService(new MembershipRepository());
+        protected busyHandler;
 
         public initialize() {
 
@@ -45,6 +46,9 @@ module Orckestra.Composer {
         public changePassword(actionContext: IControllerActionContext): void {
 
             actionContext.event.preventDefault();
+            if(this.busyHandler && this.busyHandler.isLoading())  return;
+            
+            this.busyHandler = this.asyncBusy({elementContext: actionContext.elementContext});
 
             var formData: any = this.getFormData(actionContext);
             var returnUrlQueryString: string = 'ReturnUrl=';
@@ -55,12 +59,11 @@ module Orckestra.Composer {
                 returnUrl = window.location.href.substring(window.location.href.indexOf(returnUrlQueryString)
                     + returnUrlQueryString.length);
             }
-
-            var busy = this.asyncBusy({elementContext: actionContext.elementContext});
+           
 
             this.membershipService.changePassword(formData, returnUrl)
                 .then(result => this.onChangePasswordFulfilled(result), reason => this.renderFormErrorMessages(reason))
-                .fin(() => busy.done())
+                .fin(() => this.busyHandler.done())
                 .done();
         }
 
