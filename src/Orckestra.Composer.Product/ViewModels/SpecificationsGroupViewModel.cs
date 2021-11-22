148d8ab5613e336955c475ec50cc06eb7d6fce73
Olya Ignatenko <olga.ignatenko@orckestra.com>
2021-11-22 11:18:23 +0200

    Added GroupName to SpecificationsGroupViewModel

--- src/Orckestra.Composer.Product/ViewModels/SpecificationsGroupViewModel.cs
+++ src/Orckestra.Composer.Product/ViewModels/SpecificationsGroupViewModel.cs
@@ -6,6 +6,7 @@ namespace Orckestra.Composer.Product.ViewModels
     public sealed class SpecificationsGroupViewModel : BaseViewModel
     {
         public string Title { get; set; }
+        public string GroupName { get; set; }
 
         public List<SpecificationsAttributeViewModel> Attributes { get; set; }
 
