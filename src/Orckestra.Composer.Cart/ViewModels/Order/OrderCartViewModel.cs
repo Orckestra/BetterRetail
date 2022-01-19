20127e8508cf760d329cd3ca324c58fa64031b83
Olya Ignatenko <olga.ignatenko@orckestra.com>
2022-01-19 16:29:13 +0200

    removed not need file

--- src/Orckestra.Composer.Cart/ViewModels/Order/OrderCartViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Orckestra.Overture.ServiceModel.Orders;
-
-namespace Orckestra.Composer.Cart.ViewModels.Order
-{
-    public class OrderCartViewModel {
-        public OrderItem OrderItem { get; set; }
-        public Overture.ServiceModel.Orders.Cart Cart { get; set; }
-    }
-}
