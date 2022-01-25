06fb4123589d396e1ccada7a8b646b4d44203333
Alex Honcharuk <honcharuk.alexander@gmail.com>
2022-01-25 13:22:11 +0200

    searchProductClick

--- src/Orckestra.Composer.Website/UI.Package/Typescript/Composer.Product/ProductSearch/SearchResultsController.ts
+++ src/Orckestra.Composer.Website/UI.Package/Typescript/Composer.Product/ProductSearch/SearchResultsController.ts
@@ -92,75 +92,24 @@ module Orckestra.Composer {
 
                             self.eventHub.publish(SearchEvents.FacetsLoaded, { data: result });
                         });
+                    },
+                    searchProductClick(product, index): void {
+                        self.sendProductClickForAnalytics(product, index, this.Pagination.CurrentPage)
                     }
                 }
             });
         }
 
-        public addToCart(actionContext: IControllerActionContext) {
-            console.log(actionContext);
-            var productContext: JQuery = $(actionContext.elementContext).closest('[data-product-id]');
+        protected sendProductClickForAnalytics(product, index, currentPage): void {
+            const productData = {
+                Product: product,
+                ListName: this.context.viewModel.ListName,
+                Index: index,
+                PageNumber: currentPage.DisplayName,
+                MaxItemsPerPage: this.context.viewModel.MaxItemsPerPage
+            };
 
-            var hasVariants: string = <any>productContext.data('hasVariants');
-
-            //Do not use .data since it may parse the id as a number.
-            var productId: string = productContext.attr('data-product-id');
-            var variantId: string = productContext.attr('data-product-variant-id');
-            var recurringOrderProgramName: string = productContext.attr('data-recurring-order-program-name');
-
-            var product = _.find(this.context.viewModel.SearchResults, function (product: any) {
-                if (_.isEmpty(variantId)) {
-                    return product.ProductId === productId;
-                } else {
-                    return product.ProductId === productId && product.VariantId === variantId;
-                }
-            });
-            var price: number = product.IsOnSale ? product.Price : product.ListPrice;
-
-            var busy = this.asyncBusy({ elementContext: actionContext.elementContext, containerContext: productContext });
-
-            if (hasVariants === 'True') {
-
-                this.productService.loadQuickBuyProduct(productId, variantId, 'productSearch', this.context.viewModel.ListName)
-                    .then((data: any) => {
-                        ErrorHandler.instance().removeErrors();
-                        return data;
-                    }, (reason: any) => this.onAddToCartFailed(reason))
-                    .fin(() => busy.done());
-
-            } else {
-             //   var productData: any = this.getProductDataForAnalytics(productId, price);
-            //    this.eventHub.publish('lineItemAdding', { data: productData });
-
-                this.cartService.addLineItem(productId, '' + price, null, 1, null, recurringOrderProgramName)
-                    .then((data: any) => {
-                        ErrorHandler.instance().removeErrors();
-                        return data;
-                    }, (reason: any) => this.onAddToCartFailed(reason))
-                    .fin(() => busy.done());
-            }
-        }
-
-        protected onAddToCartFailed(reason: any): void {
-            console.error('Error on adding item to cart', reason);
-
-            ErrorHandler.instance().outputErrorFromCode('AddToCartFailed');
-        }
-
-        public searchProductClick(actionContext: IControllerActionContext) {
-            var index: number = <any>actionContext.elementContext.data('index');
-            var productId: string = actionContext.elementContext.data('productid').toString();
-            var product: any = _.find(this.context.viewModel.SearchResults, { ProductId: productId });
-
-            this.eventHub.publish('productClick', {
-                data: {
-                    Product: product,
-                    ListName: this.context.viewModel.ListName,
-                    Index: index,
-                    PageNumber: this.currentPage.DisplayName,
-                    MaxItemsPerPage: this.context.viewModel.MaxItemsPerPage
-                }
-            });
+            this.eventHub.publish('productClick', { data: productData });
         }
 
         protected sendProductDataForAnalytics(product: any, price: any, list: string): void {
