8906c0877b9f1dfdfd1c4f939c6fb576fd0a0f3c
Taras Nakonechnyi <taras.nakonechnyi@orckestra.com>
2022-03-03 14:26:49 +0200

    Customer Condition Provider

---
+++ src/Orckestra.Composer.CustomerConditionProvider/Repositories/CustomerDefinitionsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orckestra.Overture;
+using Orckestra.Overture.Caching;
+using Orckestra.Overture.ServiceModel.Metadata;
+using Orckestra.Overture.ServiceModel.Requests.Metadata;
+
+namespace Orckestra.Composer.CustomerConditionProvider.Repositories
+{
+    public class CustomerDefinitionsRepository : ICustomerDefinitionsRepository
+    {
+        protected IOvertureClient OvertureClient { get; }
+        protected ICacheProvider CacheProvider { get; }
+
+        public CustomerDefinitionsRepository(IOvertureClient overtureClient, ICacheProvider cacheProvider)
+        {
+            OvertureClient = overtureClient ?? throw new ArgumentNullException(nameof(overtureClient));
+            CacheProvider = cacheProvider ?? throw new ArgumentNullException(nameof(cacheProvider));
+        }
+        public virtual async Task<EntityDefinition> GetCustomerDefinitionAsync()
+        {
+            var definition = await OvertureClient.SendAsync(new GetCustomerDefinitionRequest()
+            {
+                Name = "CUSTOMER"
+            }).ConfigureAwait(false);
+
+            return definition;
+        }
+    }
+}
