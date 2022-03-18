66f4baaa55735aa453f88997d90ef8a7a0b00f47
Taras Nakonechnyi <taras.nakonechnyi@orckestra.com>
2022-03-18 14:49:30 +0200

    Features/51383 custom content per profile attribute (#179)
    * add personalized content
    * Customer Condition Provider

---
+++ src/Orckestra.Composer.CustomerConditionProvider/StartupHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Hosting;
+using Composite.Core.Application;
+using Microsoft.Extensions.DependencyInjection;
+using Orckestra.Composer.CustomerConditionProvider.Repositories;
+using Orckestra.Tools.ConditionalContent.Providers;
+
+namespace Orckestra.Composer.ConditionalContentProvider
+{
+    [ApplicationStartup(AbortStartupOnException = true)]
+    public static class StartupHandler
+    {
+        public static void OnBeforeInitialize()
+        {
+
+        }
+
+        public static void OnInitialized()
+        {
+            if (!HostingEnvironment.IsHosted) return;
+
+        }
+
+        public static void ConfigureServices(IServiceCollection collection)
+        {
+            if (!HostingEnvironment.IsHosted) return;
+
+            collection.AddTransient<IConditionProvider, CustomerConditionProvider.Providers.CustomerConditionProvider>();
+            collection.AddTransient<ICustomerDefinitionsRepository, CustomerDefinitionsRepository>();
+        }
+    }
+}
