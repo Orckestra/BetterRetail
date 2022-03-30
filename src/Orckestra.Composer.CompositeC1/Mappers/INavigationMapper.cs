9412b59a8d57158bf55e7a60271becd01561f92e
Inna Boitsun <inna.boitsun@orckestra.com>
2022-03-30 10:35:37 +0300

    Sync/better retail (#181)
    Feature/57605 ability to have external menu items provider (#185)
    
    Ability to have external Main Menu Items provider

--- src/Orckestra.Composer.CompositeC1/Mappers/INavigationMapper.cs
+++ src/Orckestra.Composer.CompositeC1/Mappers/INavigationMapper.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using Orckestra.Composer.CompositeC1.DataTypes.Navigation;
+using Orckestra.Composer.CompositeC1.Providers.MainMenu;
 using Orckestra.Composer.ViewModels.Home;
 
 namespace Orckestra.Composer.CompositeC1.Mappers
 {
     public interface INavigationMapper
     {
-        IEnumerable<IMenuEntryViewModel> MapMainMenuItems(List<MainMenu> mainMenuItems, Guid? parentId = null);
+        IEnumerable<IMenuEntryViewModel> MapMainMenuItems(List<MainMenuItemWrapper> mainMenuItems, Guid? parentId = null);
         HomeNavigationImageViewModel MapNavigationImage(NavigationImage navigationImage);
         IEnumerable<IMenuEntryViewModel> MapHeaderOptionalLinksItems(List<HeaderOptionalLink> optionalLinksItems);
         IEnumerable<IMenuEntryViewModel> MapFooterOptionalLinksItems(List<FooterOptionalLink> optionalLinksItems);
