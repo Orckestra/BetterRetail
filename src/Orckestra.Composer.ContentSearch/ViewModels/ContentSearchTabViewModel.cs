90c5d8a7bc52769abde8b6844f1ed6c2c8324633
vadym-hyryn <57723696+vadym-hyryn@users.noreply.github.com>
2022-04-05 18:37:02 +0300

    Add ContentSearchTabViewModel property (#191)

--- src/Orckestra.Composer.ContentSearch/ViewModels/ContentSearchTabViewModel.cs
+++ src/Orckestra.Composer.ContentSearch/ViewModels/ContentSearchTabViewModel.cs
@@ -12,5 +12,6 @@ namespace Orckestra.Composer.ContentSearch.ViewModels
         public int PagesCount { get; set; }
         public bool IsProducts { get; set; }
         public bool IsActive { get; set; }
+        public string DataTypes { get; set; }
     }
 }
