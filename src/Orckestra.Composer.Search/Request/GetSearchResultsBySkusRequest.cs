fee2adb684ca4f7f2512717803bd71bd510be374
Anstsiya <anastasiya.yenina@gmail.com>
2022-04-07 11:56:14 +0300

    API to Search Products by SKUs (#194)
     API to Search Products by SKUs
    
    Co-authored-by: Anastasiia Yenina <anastasiia.yenina@orckestra.com>

---
+++ src/Orckestra.Composer.Search/Request/GetSearchResultsBySkusRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orckestra.Composer.Search.Request
+{
+    public class GetSearchResultsBySkusRequest
+    {
+        [Required]
+        public string[] Skus { get; set; }
+
+        public string QueryString { get; set; }
+
+        public bool IncludeFacets { get; set; }
+    }
+}
