704b16d31bc13340fd69a3b2beb3c9573397e87f
Dmitry Dzygin <dmitry.dzygin@orckestra.com>
2022-12-12 13:02:41 +0100

    Improving performance of reading website settings from the page metadata

--- src/Orckestra.Composer.CompositeC1/Settings/RecurringOrdersSettings.cs
+++ src/Orckestra.Composer.CompositeC1/Settings/RecurringOrdersSettings.cs
@@ -1,89 +1,27 @@
-﻿using Composite.Data;
+﻿using System;
 using Orckestra.Composer.Configuration;
 using Orckestra.Composer.Services;
-using System;
-using System.Linq;
 
 namespace Orckestra.Composer.CompositeC1.Settings
 {
     public class RecurringOrdersSettings : IRecurringOrdersSettings
     {
-        public IWebsiteContext WebsiteContext;
-        private DataTypes.RecurringOrdersSettings RecurringOrdersSettingsMeta;
+        private readonly Lazy<DataTypes.RecurringOrdersSettings> _recurringOrdersSettingsMeta;
+        private DataTypes.RecurringOrdersSettings RecurringOrdersSettingsMeta => _recurringOrdersSettingsMeta.Value;
 
         public RecurringOrdersSettings(IWebsiteContext websiteContext)
         {
-            WebsiteContext = websiteContext;
-            using(var con = new DataConnection())
-            {
-                RecurringOrdersSettingsMeta = con.Get<DataTypes.RecurringOrdersSettings>().FirstOrDefault(g => g.PageId == WebsiteContext.WebsiteId);
-            }
+            _recurringOrdersSettingsMeta = new Lazy<DataTypes.RecurringOrdersSettings>(websiteContext.GetRootPageMetaData<DataTypes.RecurringOrdersSettings>);
         }
 
-        public bool Enabled
-        {
-            get
-            {
-                if (RecurringOrdersSettingsMeta != null)
-                {
-                    return RecurringOrdersSettingsMeta.Enabled;
-                }
+        public bool Enabled => RecurringOrdersSettingsMeta?.Enabled ?? false;
 
-                return false;
-            }
-        }
+        public Guid RecurringCartDetailsPageId => RecurringOrdersSettingsMeta?.RecurringCartDetailsPageId ?? Guid.Empty;
 
-        public Guid RecurringCartDetailsPageId
-        {
-            get
-            {
-                if (RecurringOrdersSettingsMeta != null && RecurringOrdersSettingsMeta.RecurringCartDetailsPageId.HasValue)
-                {
-                    return RecurringOrdersSettingsMeta.RecurringCartDetailsPageId.Value;
-                }
+        public Guid RecurringCartsPageId => RecurringOrdersSettingsMeta?.RecurringCartsPageId ?? Guid.Empty;
 
-                return Guid.Empty;
-            }
-        }
+        public Guid RecurringScheduleDetailsPageId => RecurringOrdersSettingsMeta?.RecurringScheduleDetailsPageId ?? Guid.Empty;
 
-        public Guid RecurringCartsPageId
-        {
-            get
-            {
-                if (RecurringOrdersSettingsMeta != null && RecurringOrdersSettingsMeta.RecurringCartsPageId.HasValue)
-                {
-                    return RecurringOrdersSettingsMeta.RecurringCartsPageId.Value;
-                }
-
-                return Guid.Empty;
-            }
-        }
-
-        public Guid RecurringScheduleDetailsPageId
-        {
-            get
-            {
-                if (RecurringOrdersSettingsMeta != null && RecurringOrdersSettingsMeta.RecurringScheduleDetailsPageId.HasValue)
-                {
-                    return RecurringOrdersSettingsMeta.RecurringScheduleDetailsPageId.Value;
-                }
-
-                return Guid.Empty;
-            }
-        }
-
-        public Guid RecurringSchedulePageId
-        {
-            get
-            {
-                if (RecurringOrdersSettingsMeta != null && RecurringOrdersSettingsMeta.RecurringSchedulePageId.HasValue)
-                {
-                    return RecurringOrdersSettingsMeta.RecurringSchedulePageId.Value;
-                }
-
-                return Guid.Empty;
-            }
-        }
-
+        public Guid RecurringSchedulePageId => RecurringOrdersSettingsMeta?.RecurringSchedulePageId ?? Guid.Empty;
     }
 }
--- src/Orckestra.Composer/Services/IWebsiteContext.cs
+++ src/Orckestra.Composer/Services/IWebsiteContext.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Composite.Data;
 
 namespace Orckestra.Composer.Services
 {
     public interface IWebsiteContext
     {
+        /// <summary>
+        /// The ID of the root page of the current website.
+        /// </summary>
         Guid WebsiteId { get; }
+
+        /// <summary>
+        /// Gets the meta data attached to the root page of the current website.
+        /// </summary>
+        T GetRootPageMetaData<T>() where T : class, IPageMetaData;
     }
 }
