ab18b87d9b2e916d98a2fd747b1b27417b8f57b8
Olya Ignatenko <olya.ignatenko@orckestra.com>
2024-11-28 13:52:53 +0200

    added pageHelper

---
+++ src/Orckestra.Composer/Utils/PageHelper.cs
@@ -0,0 +1,21 @@
+﻿using Composite.Plugins.Components.ComponentTags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Composite.Data;
+
+namespace Orckestra.Composer.Utils
+{
+    public static class PageHelper
+    {
+        public static Dictionary<string, string> GetWebsitesRootPages()
+        {
+            return PageManager.GetChildrenIDs(Guid.Empty)
+                .Select(rootPageId => PageManager.GetPageById(rootPageId))
+                .Where(page => page != null)
+                .ToDictionary(page => page.Id.ToString(), page => page.Title);
+        }
+    }
+}
