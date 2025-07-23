5b36330f4b025f91de728ff50a4fc6bf5962124b
Inna Boitsun <inna.boitsun@orckestra.com>
2025-07-23 16:20:07 +0300

    removed ConfigureAwait(false): is used to avoid resuming on the original synchronization context — which is helpful in async methods (e.g., await ...ConfigureAwait(false)). But we are not awaiting this code —  .GetAwaiter().GetResult() to force sync execution. Since the method is already blocking, ConfigureAwait(false) has no meaningful impact here.

--- src/Orckestra.Composer.CompositeC1/Controllers/BrowsingCategoriesBaseController.cs
+++ src/Orckestra.Composer.CompositeC1/Controllers/BrowsingCategoriesBaseController.cs
@@ -45,13 +45,14 @@ namespace Orckestra.Composer.CompositeC1.Controllers
             [Bind(Prefix = SearchRequestParams.SortDirection)] string sortDirection = null)
         {
             return ExecuteBrowsingAsync("CategoryBrowsingSummaryEmpty", "CategoryBrowsingSummary", c => c, null, page, sortBy, sortDirection)
-                .ConfigureAwait(false).GetAwaiter().GetResult();
+                .GetAwaiter().GetResult();
         }
+
 
         public virtual ActionResult ChildCategories(int page = 1, string sortBy = null, string sortDirection = null)
         {
             return ExecuteBrowsingAsync("ChildCategories", "ChildCategories", c => c, EmptyCategoryBrowsingContainer, page, sortBy, sortDirection)
-                .ConfigureAwait(false).GetAwaiter().GetResult();
+                .GetAwaiter().GetResult();
         }
 
         protected async Task<ActionResult> ExecuteBrowsingAsync(string emptyView, string filledView, Func<CategoryBrowsingViewModel, object> viewModelSelector, object emptyViewModel, int page, string sortBy = null, string sortDirection = null)
