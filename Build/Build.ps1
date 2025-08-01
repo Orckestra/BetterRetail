954a4fb0b94437fc89aeb989a5171cabeafb3434
Martin Dufresne <martin.dufresne@orckestra.com>
2025-08-01 14:33:42 -0400

    $env:DOTNET_*

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -214,6 +214,9 @@ Try
         Pop-Location
     }
 
+    $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
+    $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
+    $env:DOTNET_NOLOGO = '1'
     dotnet tool restore
     if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
