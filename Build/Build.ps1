eaab9ef988b5d3e652502a13ed3df751279da37e
Martin Dufresne <martin.dufresne@orckestra.com>
2025-08-01 14:24:26 -0400

    Debug, line by line

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -232,7 +232,7 @@ Try
 
     # Start Cake
     Write-Host "Running build script..."
-    dotnet cake build.cake --verbosity=diagnostic -- $cakeArguments
+    #dotnet cake build.cake --verbosity=diagnostic -- $cakeArguments
     Write-Host "dotnet cake build.cake was called."
 }
 Finally
